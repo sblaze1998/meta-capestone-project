486d635876fac9523f3a1fe6836d657f7ca0345b
sblaze1998 <satvik.jain.cse@gmail.com>
2025-04-21 20:23:19 +0530

    created components

---
+++ capestone-project/src/components/reservation/ConfirmCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import reservationCss from "../../styles/reservations.module.css";
+
+const ConfirmCard = () => {
+  const { confirm_card } = reservationCss;
+  return (
+    <article className={confirm_card}>
+      <h1>Your Reservation has been confirmed</h1>
+      <h2>
+        Check your Email <i className="fa-regular fa-envelope"></i>
+      </h2>
+    </article>
+  );
+};
+
+export default ConfirmCard;
