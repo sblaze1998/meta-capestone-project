d524f51c70d244d62f05e72545774fc1f0c09d7d
sblaze1998 <satvik.jain.cse@gmail.com>
2025-04-21 21:32:51 +0530

    added setup for jest

---
+++ capestone-project/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  collectCoverage: false,
+  testEnvironment: 'jsdom',
+  // coverageDirectory: 'coverage',
+  // coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  // // Include the paths to your source files
+  // collectCoverageFrom: [
+  //   'src/**/*.{js,jsx,ts,tsx}',
+  //   '!src/**/*.d.ts'
+  // ],
+  testTimeout: 50000,
+};
